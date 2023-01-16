82146f306103bd9ab3473bc337a48453c86905ba
evnwttn <evnwttn@gmail.com>
2023-01-16 16:05:30 -0330

    40

--- jello/src/app/components/tasks/tasks.component.ts
+++ jello/src/app/components/tasks/tasks.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { dummyTasks } from 'src/app/dummy/dummyTasks';
+import { Task } from 'src/app/types/Task';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent {
-
+  tasks: Task[] = dummyTasks;
 }
