884828304f641a0d81953bdf3f4f9d7bb0b53ece
evnwttn <evnwttn@gmail.com>
2023-01-16 15:19:00 -0330

    dummytask / Task type

---
+++ jello/src/app/dummy/dummy-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from '../types/Task';
+
+export const dummyTasks: Task[] = [
+  {
+    id: 1,
+    task: 'PTA Meeting',
+    day: 'Jan 10',
+    reminder: true,
+  },
+  {
+    id: 2,
+    task: 'Dentist Appointment',
+    day: 'Jan 11',
+    reminder: false,
+  },
+  {
+    id: 3,
+    task: 'Job Interview',
+    day: 'Jan 12',
+    reminder: true,
+  },
+];
