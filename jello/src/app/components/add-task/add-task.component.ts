0965a2f9eb4af4d3ae257f10729e28d60ccf15e3
evnwttn <evnwttn@gmail.com>
2023-01-17 12:43:43 -0330

    :heavy_check_mark:

--- jello/src/app/components/add-task/add-task.component.ts
+++ jello/src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from 'src/app/types/Task';
 
 @Component({
   selector: 'app-add-task',
@@ -6,7 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent {
-  task!: string;
-  day!: string;
-  reminder!: boolean;
+  task: string = '';
+  day: string = '';
+  reminder: boolean = false;
+
+  onSubmit() {
+    if (this.task === '') {
+      alert('Please add a task');
+      return;
+    }
+
+    const newTask: Task = {
+      task: this.task,
+      day: this.day,
+      reminder: this.reminder,
+
+      // emit task
+    };
+  }
 }
