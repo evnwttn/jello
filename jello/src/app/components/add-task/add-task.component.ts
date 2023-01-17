8d6ab880902f7221d368300a77e494f490ba590c
evnwttn <evnwttn@gmail.com>
2023-01-17 14:59:29 -0330

    add task toggle

--- jello/src/app/components/add-task/add-task.component.ts
+++ jello/src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
 import { Task } from 'src/app/types/Task';
 
 @Component({
@@ -11,6 +13,14 @@ export class AddTaskComponent {
   task: string = '';
   day: string = '';
   reminder: boolean = false;
+  showAddTask: boolean = false;
+  subscription: Subscription = new Subscription();
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   onSubmit() {
     if (this.task === '') {
--- jello/src/app/components/header/header.component.ts
+++ jello/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +9,16 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   title: string = 'jello!!!';
+  showAddTask: boolean = false;
+  subscription: Subscription = new Subscription();
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   toggleAddTask() {
-    console.log('toggle add task');
+    this.uiService.toggleAddTask();
   }
 }
